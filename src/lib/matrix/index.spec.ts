c5b348a3b0dbb26872228e3678c8a7f1fd070ee6
jan-macbook <navarrojanderrick@gmail.com>
2020-11-28 23:46:27 -0800

    feat/ created matrix class

--- src/lib/matrix/index.spec.ts
+++ src/lib/matrix/index.spec.ts
@@ -2,26 +2,68 @@ import { Matrix } from './';
 
 describe('Matrix', () => {
   describe('constructor', () => {
+    let mat: Matrix;
+    beforeEach(() => {
+      mat = new Matrix([1,1]);
+    });
     it('should create', () => {
-      expect(new Matrix([1,1])).toBeTruthy();
+      expect(mat).toBeTruthy();
     });
     it('should create with dimensions given', () => {
-      const mat = new Matrix([1,1]);
       expect(mat.rows).toBe(1);
       expect(mat.cols).toBe(1);
+    });
+    it('should have 0 cost on creation', () => {
+      expect(mat.cost).toBe(0);
     });
   });
 
   describe('getters', () => {
-    describe('cost', () => {
+    describe('dimensions', () => {
       let matrix: Matrix;
       beforeEach(() => {
         matrix = new Matrix([1,1]);
+      });
+      it('should return correct dimensions', () => {
+        expect(matrix.dimensions).toEqual([1,1]);
       });
     });
   });
 
   describe('methods', () => {
 
+  });
+
+  describe('static', () => {
+    describe('multiply', () => {
+      it('should throw error if multiplication is not allowed', () => {
+        expect(() => {
+          Matrix.multipy(new Matrix([1,1]), new Matrix([2,2]));
+        }).toThrowError();
+      });
+      it('should return a new matrix', () => {
+        const A = new Matrix([1,1]);
+        const B = new Matrix([1,1]);
+        const prod = Matrix.multipy(A, B);
+        expect(prod).toBeInstanceOf(Matrix);
+      });
+      it('should return a new matrix with correct calculated cost', () => {
+        let A = new Matrix([4,2]);
+        let B = new Matrix([2,5]);
+        let prod = Matrix.multipy(A, B);
+        expect(prod.cost).toEqual(4*2*5);
+
+        A.cost = 10;
+        B.cost = 20;
+        prod = Matrix.multipy(A, B);
+        expect(prod.cost).toEqual(10+20+4*2*5);
+      });
+      it('should return a new matrix with correct dimensions', () => {
+        const A = new Matrix([4,2]);
+        const B = new Matrix([2,5]);
+        const prod = Matrix.multipy(A, B);
+        expect(prod.dimensions).toEqual([4,5]);
+      });
+    });
   });
 });
--- src/lib/matrix/index.ts
+++ src/lib/matrix/index.ts
@@ -1,3 +1,7 @@
+export interface MatrixHash {
+  [key: string]: Matrix;
+};
+
 export interface MatrixDimensions {
   [0]: number;
   [1]: number;
@@ -6,20 +10,39 @@ export interface MatrixDimensions {
 export interface MatrixInterface {
   rows: number;
   cols: number;
+  cost: number;
+};
+
+export class MatrixMultError extends Error {
+  constructor() {
+    super('Matrix multiplication forbidden.');
+    this.name = 'MatrixMultError';
+  };
 };
 
 export class Matrix implements MatrixInterface {
   rows: number;
   cols: number;
+  cost: number;
+
   constructor(dimensions: MatrixDimensions) {
     this.rows = dimensions[0];
     this.cols = dimensions[1];
+    this.cost = 0;
   };
 
-  static multipy(M: Matrix, N: Matrix) {
-    if (M.cols !== N.rows) return;
-    const result = new Matrix([M.rows, N.cols]);
-    
+  get dimensions(): MatrixDimensions {
+    return [this.rows, this.cols];
+  };
+
+  print(): void {
+    console.log(`${this.rows}\t${this.cols}\t${this.cost}`)
+  };
+
+  static multipy(A: Matrix, B: Matrix) {
+    if (A.cols !== B.rows) throw new MatrixMultError();
+    const result = new Matrix([A.rows, B.cols]);
+    result.cost = A.cost + B.cost + (A.rows * A.cols * B.cols);
     return result;
   };
 };
