95c9d939e2f626f8bc4495e56acca4ce1c300eea
jan-macbook <navarrojanderrick@gmail.com>
2021-01-04 09:04:42 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import { parenthesize } from './lib/parenthesize';
-
-const results = parenthesize('ABC');
-// console.log(results);
-
-for (let res of results) {
-  const parenRegex = /\((.+?)\)/;
-  const regexResult = res.match(parenRegex);
-  console.log(regexResult?.[1]);
-}
-// create the matrices for A, B, C
-// parenthesize ABC to get all the possible multiplications
-// for each combination do the multiplication and store it
-
+const parenRegex = /\((.+?)\)/;
+const results = parenthesize('ABCDEF');
+console.log(results);
